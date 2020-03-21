d7925f9e934e9e75c2b8c2614cb50a578735bdf5
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-03-21 18:15:33 +0300

    fix: add code at index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configs = bracketsConfig.map(element => element.join(''));
+  for (let i = 0; i < configs.length; i++) {
+    let index = str.indexOf(configs[i])
+    if (index !== -1) {
+      str = str.slice(0, index) + str.slice(index + 2);
+      i = -1;
+    }
+  }
+  if (!str) {
+    return true;
+  } else {
+    return false;
+  }
 }
